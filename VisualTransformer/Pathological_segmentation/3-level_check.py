e6af36dfd38f0a9a7fa96177bdd04c7101be3a69
Xiaokang Wu <24110740024@m.fudan.edu.cn>
2026-02-09 23:10:23 +0800

    Delete VisualTransformer/Pathological_segmentation directory
    delete pathological_segmentation

--- VisualTransformer/Pathological_segmentation/3-level_check.py
+++
@@ -1,8 +0,0 @@
-import openslide
-
-wsi_path = r'.\wsi_file\22-A-C-1HE_Wholeslide_Default_Extended.tif'
-wsi = openslide.OpenSlide(wsi_path)
-
-print(f"Number of levels: {wsi.level_count}")
-print(f"Level dimensions: {wsi.level_dimensions}")
-print(f"Level downsamples: {wsi.level_downsamples}")
